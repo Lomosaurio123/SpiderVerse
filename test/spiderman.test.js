f1136f2d75e1326582e2c92757b4ccadddbf958e
Mauricio Gael Hernandez Lom <mhernandezl1605@alumno.ipn.mx>
2022-04-28 00:43:11 -0500

    Paso 3: Crear la prueba de unidad con lo que se espera desarrollar

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,6 +1,12 @@
-describe("Test suite Dummy Description", () => {
-    test('Case 1 Dummy', () => {
-        const resultOfSomething = 1 + 2
-        expect(resultOfSomething).toBe(18)
+describe("Unit Test for Spiderman class", () => {
+    test('1) Create an spiderman object', () => {
+        const andrewGarfield = new Spiderman("Spiderman Sony", 31, "Andrew Garfield", 2, "Sony")
+        
+        //Validamos que este código funcione adecuadamente
+        expect(andrewGarfield.name).toBe("Spiderman Sony")
+        expect(andrewGarfield.age).toBe(31)
+        expect(andrewGarfield.actor).toBe("Andrew Garfield")
+        expect(andrewGarfield.movies).toBe(2)
+        expect(andrewGarfield.studio).toBe("Sony")
     })
 })
